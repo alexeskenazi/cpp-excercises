12d45045e8b24ce61e6241971adecde113bfc9d1
alexeskenazi <112673600+alexeskenazi@users.noreply.github.com>
2025-01-18 09:00:28 -0500

    Enabling all tests and simplifying the texmplate

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 #include "valid-parentheses.h"
 #include "mergelist.h"
 #include "buy-chocolate.h"
+#include "two-sum.h"
 
 
 
 int main() {
 
+    TwoSum ts;
+    ts.checks();
+
     Chocolate choc;
     choc.checks();
 
-    // ValidParentheses vp;
-    // vp.checks();
+    ValidParentheses vp;
+    vp.checks();
 
 
-    // MergeList ml;
-    // ml.checks();
+    MergeList ml;
+    ml.checks();
   
     return 0;
 }
